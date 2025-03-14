ec47b6b06a9538fdca89d0684f4ec96035ded49d
xed96rus@gmail.com <maksim@HOME-PC>
2025-03-14 20:15:37 +0300

    Second changes, A*

--- custom_msgs/install/custom_msgs/local/lib/python3.10/dist-packages/custom_msgs/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from custom_msgs.msg._pose2_d import Pose2D  # noqa: F401
--- ros2_pose/build/move_with_obstacle/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/maksim/ros2_pose/install/move_with_obstacle'
